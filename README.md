00336956934a42367a92a03ab400a3ff29a14e76
Albert Montolio Romo <albertmonro@gmail.com>
2017-10-15 21:53:55 +0200

    Improve the feature to avoid an extra reload. Update Readme

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # simkl-enhancer
+
+Simkl-enhancer is a chrome extension to add some funcionalities to [Simkl](https://simkl.com) a TV shows, Movies and Animes Tracker.
+
+## Funcionalities
+
+* Add an https://animeflv.net direct link in all the episodes o animes. There are some that doesn't have link and is posible that some don't work. 
+
+![image](https://imgur.com/QoR78uc "Example of the feature")
+
+## Instalation
+
+Clone or Download the code in any folder. After that go to  `chrome://extensions/` in your browser and drag and drop the folder containg all the files
--- content.js
+++ content.js
@@ -1,26 +1,30 @@
-console.log("animeflv executed");
-var episodeLinkElements = document.getElementsByClassName("SimklTVDetailEpisodeLinksItem");
+window.addEventListener("lprequestend", function(){addAlternativeLinks();});
 
-const episodeRegexp = /\/([^\/]*)\/episode-(\d+)/g;
+function addAlternativeLinks(){
+    const episodeRegexp = /https:\/\/simkl.com\/anime\/\d+\/([^\/]*)\/episode-(\d+)/g;
 
-let info = episodeRegexp.exec(window.location.href);
+    let info = episodeRegexp.exec(window.location.href);
+    if(info !== null){
+        const episodeLink = "https://animeflv.net/ver/1/" + info[1] + "-" + info[2];
 
-const episodeLink = "https://animeflv.net/ver/1/" + info[1] + "-" + info[2];
+        let newLinkHtml = 
+        "<div class=\"SimklTVDetailEpisodeLinksItem Sub ajLinkInside SimklTVDetailEpisodeLinksItemAnimeFLV\" id=\"emb0\">" +
+            "<a target=\"_blank\" href=\"" + episodeLink + "\">"+
+            "<table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\">" +
+                "<tbody><tr>"        +
+                    "<td width=\"1\" class=\"SimklTVDetailEpisodeLinkNumber\">Sub<\/td>"+
+                    "<td width=\"1\"><div class=\"SimklTVDetailEpisodeLinkAnimeFLV\">&nbsp;<\/div><\/td>"+
+                    "<td class=\"SimklTVDetailEpisodeLinkDesc\">"+
+                        "Free full episode available. Click here to watch now.<br>"+
+                    "<\/td>"+
+                    "<td width=\"1\" class=\"SimklTVDetailEpisodeLinkGo\">\u00BB<\/td>"+
+                "<\/tr><\/tbody>" +
+            "<\/table>"+
+             "<\/a>"+
+        "<\/div>";
 
-let newLinkHtml = 
-"<div class=\"SimklTVDetailEpisodeLinksItem Sub ajLinkInside SimklTVDetailEpisodeLinksItemAnimeFLV\" id=\"emb0\">" +
-    "<a target=\"_blank\" href=\"" + episodeLink + "\">"+
-    "<table width=\"100%\" border=\"0\" cellspacing=\"0\" cellpadding=\"0\">" +
-        "<tbody><tr>"        +
-            "<td width=\"1\" class=\"SimklTVDetailEpisodeLinkNumber\">Sub<\/td>"+
-            "<td width=\"1\"><div class=\"SimklTVDetailEpisodeLinkAnimeFLV\">&nbsp;<\/div><\/td>"+
-            "<td class=\"SimklTVDetailEpisodeLinkDesc\">"+
-                "Free full episode available. Click here to watch now.<br>"+
-            "<\/td>"+
-            "<td width=\"1\" class=\"SimklTVDetailEpisodeLinkGo\">\u00BB<\/td>"+
-        "<\/tr><\/tbody>" +
-    "<\/table>"+
-     "<\/a>"+
-"<\/div>";
-
-episodeLinkElements[episodeLinkElements.length-1].insertAdjacentHTML("afterend", newLinkHtml);
+        var episodeLinkElements = document.getElementsByClassName("SimklTVDetailEpisodeLinksItem");
+        episodeLinkElements[episodeLinkElements.length-1].insertAdjacentHTML("afterend", newLinkHtml);
+    }
+    
+}
